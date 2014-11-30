1e547ed62ce7b24e7af719a0933912b98046d5e8
jrad <behradz@gmail.com>
2014-11-30 16:02:27 +0330

    add flash jwplayer

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ module.exports = {
   couchdb: {
     host    : process.env.VANALYTICS_COUCH_HOST || "127.0.0.1",
     port    : process.env.VANALYTICS_COUCH_HOST || 5984,
-    db      : process.env.VANALYTICS_COUCH_DB || "vanalytics",
-    usage_db: process.env.VANALYTICS_COUCH_DB || "access_log",
+    db      : process.env.VANALYTICS_COUCH_DB || "vanalytics", // database which video player client logs will be pushed to
+    usage_db: process.env.VANALYTICS_COUCH_DB || "access_log", // database which syslog access logs will be pushed to
     username: process.env.VANALYTICS_COUCH_USER || "admin",
     password: process.env.VANALYTICS_COUCH_PASS || "admin"
   },
