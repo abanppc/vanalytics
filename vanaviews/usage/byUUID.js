e555aa6dcbb165048b3f2e7b8bd423de40200cdf
jrad <behradz@gmail.com>
2015-01-03 18:56:16 +0330

    fixes #3

--- vanaviews/usage/byUUID.js
+++ vanaviews/usage/byUUID.js
@@ -3,15 +3,15 @@ module.exports.map = function (doc) {
   if( doc.request ) {
     //var url = doc.request.match( /\/(.*)\.(mp4|webm|flv|ts)/i );
     var url = doc.request.match( /\/([^/]+)\.ts\sHTTP/i );
-    if( url.length == 2 ) {
+    if( url && url.length == 2 ) {
       filename = url[1].replace( /-[^-]+$/, '' ).replace( /-[^-]+$/, '' );
     } else { // not a ts file
       url = doc.request.match( /\/([^/]+)\..+\sHTTP/i );
-      if( url.length == 2) {
+      if( url && url.length == 2) {
         filename = url[1].replace( /-[^-]+$/, '' );
       }
     }
-    if ( filename && filename.length == 3 ) {
+    if ( filename ) {
       var d = new Date( doc.timestamp );
       emit(
         [ filename, d.getFullYear(), d.getMonth() + 1, d.getDate(), d.getHours(), d.getMinutes() ],
